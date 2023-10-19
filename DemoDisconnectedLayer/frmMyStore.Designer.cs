3e53c9c34d66f8ddf3dd4dbd9efe29898a773c89
mdao2211 <superhero21442922@gmail.com>
2023-10-19 10:53:20 +0700

    Add project files.

---
+++ DemoDisconnectedLayer/frmMyStore.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DemoDisconnectedLayer
+{
+    partial class frmMyStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnViewProducts = new Button();
+            btnViewCategories = new Button();
+            btnClose = new Button();
+            dgvData = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // btnViewProducts
+            // 
+            btnViewProducts.Location = new Point(75, 42);
+            btnViewProducts.Name = "btnViewProducts";
+            btnViewProducts.Size = new Size(214, 56);
+            btnViewProducts.TabIndex = 0;
+            btnViewProducts.Text = "View Products";
+            btnViewProducts.UseVisualStyleBackColor = true;
+            btnViewProducts.Click += btnViewProducts_Click;
+            // 
+            // btnViewCategories
+            // 
+            btnViewCategories.Location = new Point(464, 42);
+            btnViewCategories.Name = "btnViewCategories";
+            btnViewCategories.Size = new Size(214, 56);
+            btnViewCategories.TabIndex = 1;
+            btnViewCategories.Text = "View Categories";
+            btnViewCategories.UseVisualStyleBackColor = true;
+            btnViewCategories.Click += btnViewCategories_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(265, 366);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(214, 56);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // dgvData
+            // 
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(52, 123);
+            dgvData.Name = "dgvData";
+            dgvData.RowHeadersWidth = 51;
+            dgvData.RowTemplate.Height = 29;
+            dgvData.Size = new Size(696, 225);
+            dgvData.TabIndex = 3;
+            // 
+            // frmMyStore
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvData);
+            Controls.Add(btnClose);
+            Controls.Add(btnViewCategories);
+            Controls.Add(btnViewProducts);
+            Name = "frmMyStore";
+            Text = "frmMyStore";
+            Load += frmMyStore_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnViewProducts;
+        private Button btnViewCategories;
+        private Button btnClose;
+        private DataGridView dgvData;
+    }
+}
---
+++ DemoDisconnectedLayer/frmMyStore.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoDisconnectedLayer
+{
+    public partial class frmMyStore : Form
+    {
+        public frmMyStore()
+        {
+            InitializeComponent();
+        }
+        DataSet dsMyStore = new DataSet();
+
+        private void btnViewProducts_Click(object sender, EventArgs e)
+        {
+            dgvData.DataSource = dsMyStore.Tables[0];
+        }
+
+        private void btnViewCategories_Click(object sender, EventArgs e)
+        {
+            dgvData.DataSource = dsMyStore.Tables[1];
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) => this.Close();
+
+        private void frmMyStore_Load(object sender, EventArgs e)
+        {
+            string ConnectionStrings = "Server = localhost,uid=sa,pwd=1234;database=MyStoreTrustServerCertificate=true;Trusted_Connection=SSPI;Encrypt=false;";
+            string SQL = "Select ProductID,ProductName,UnitsStock From Products ; Select * From Categories";
+            try
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(SQL, ConnectionStrings);
+                dataAdapter.Fill(dsMyStore);
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "");
+            }
+            
+        }
+    }
+}
